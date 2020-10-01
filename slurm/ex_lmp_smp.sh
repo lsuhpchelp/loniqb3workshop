0dc7c72796d14e8d5f96b64bdc152f5cefaabc2f
Feng Chen <fchen14@lsu.edu>
2020-09-30 23:41:32 -0500

    fixed input file

--- slurm/ex_lmp_smp.sh
+++ slurm/ex_lmp_smp.sh
@@ -3,7 +3,7 @@
 #SBATCH -p single                  # For jobs using more than 1 node, submit to workq
 #SBATCH --nodes=1                  # Number of nodes to be allocated
 #SBATCH --ntasks=1                 # processes
-#SBATCH --cpus-per-task=8          # Number of CPU cores per task
+#SBATCH --cpus-per-task=24          # Number of CPU cores per task
 #SBATCH --time=00:30:00            # Wall time limit (hh:mm:ss)
 #SBATCH --output=%x-%j.log         # Standard output and error 
 
@@ -20,5 +20,5 @@ module load mvapich2/2.3.3/intel-19.0.5
 module load lammps/20200303/intel-19.0.5-mvapich-2.3.3
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-/usr/bin/time $(which lmp) -in in.lmp.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
+/usr/bin/time $(which lmp) -in in.lmp.ser.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
 
